bf09e8227942a58da7f3857202895e6503cb8dd7
RhazzXIX <jm21tonio@gmail.com>
2023-03-16 14:53:09 +0800

    Add initial directories and files

---
+++ src/components/Info.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Info extends Component {
+  render() {
+    return(
+      <div>
+        Hello! Up-skilling World!
+      </div>
+    )
+  }
+};
+
+export default Info
