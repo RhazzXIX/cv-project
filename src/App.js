899e650ddda20d8612e64d28bcbf4fc9df3a8c14
RhazzXIX <jm21tonio@gmail.com>
2023-03-16 20:23:07 +0800

    Add Info component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { Component } from "react";
 import Info from "./components/Info";
 
-function App() {
-  return (
-    <div >
-      <Info />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Driven Person",
+      jobTitle: "Web Developer",
+      address: "Competence Kingdom",
+      contacts: [
+        "195-555-0375",
+        "example@email.com",
+        "github.com/user",
+        "linkedin.com/user",
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <main>
+        <Info infos={this.state} />
+      </main>
+    );
+  }
 }
 
 export default App;
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,13 +1,23 @@
 import { Component } from "react";
+import uniqid from "uniqid";
 
 class Info extends Component {
   render() {
-    return(
-      <div>
-        Hello! Up-skilling World!
-      </div>
-    )
+    const { name, jobTitle, address, contacts } = this.props.infos;
+    return (
+      <section id="baseInfo">
+        <h1 id="name">{name}</h1>
+        <h3 id="title">{jobTitle}</h3>
+        <h4 id="address">{address}</h4>
+        <ul id="contacts">
+          {contacts.map((contact, i) => {
+            if (i === 0) return <li key={uniqid()}>{contact} </li>;
+            return <li key={uniqid()}>|| {contact} </li>;
+          })}
+        </ul>
+      </section>
+    );
   }
-};
+}
 
-export default Info
+export default Info;
