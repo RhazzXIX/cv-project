24d084a49b0b130caa274a32c8cea088d4a6af78
RhazzXIX <jm21tonio@gmail.com>
2023-03-16 23:48:37 +0800

    Add methods for handling name

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
 import { Component } from "react";
 import Info from "./components/Info";
+import uniqid from "uniqid";
+
+class Element extends Component {
+  render() {
+    return <button></button>;
+  }
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "Driven Person",
-      jobTitle: "Web Developer",
+      name: {
+        text: "Driven Person",
+        edit: false,
+      },
       address: "Competence Kingdom",
+      addressEdit: {
+        text: "Competence Kingdom",
+        edit: false,
+      },
+      contact: {
+        text: "",
+        id: uniqid(),
+      },
       contacts: [
-        "195-555-0375",
-        "example@email.com",
-        "github.com/user",
-        "linkedin.com/user",
+        {
+          text: "195-555-0375",
+          id: uniqid(),
+        },
+        {
+          text: "example@email.com",
+          id: uniqid(),
+        },
+        {
+          text: "github.com/user",
+          id: uniqid(),
+        },
+        {
+          text: "linkedin.com/user",
+          id: uniqid(),
+        },
       ],
+      profile: `Always pursuing improvement, not resting in my Laurels`,
+      profileEdit: {
+        text: "",
+        edit: false,
+      },
     };
+    this.element = <button>test</button>;
   }
+
+  submitName = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    this.setState({
+      name: {
+        text: this.state.name.text,
+        edit: false,
+      },
+    });
+  };
+
+  handleName = (e) => {
+    e.stopPropagation();
+    this.setState({
+      name: {
+        text: e.target.value,
+        edit: true,
+      },
+    });
+  };
+
+  editName = (e) => {
+    e.stopPropagation();
+    this.setState({
+      name: {
+        text: this.state.name.text,
+        edit: true,
+      },
+    });
+  };
 
   render() {
     return (
       <main>
-        <Info infos={this.state} />
+        <Info
+          infos={this.state}
+          method={{
+            edit: this.editName,
+            submit: this.submitName,
+            change: this.handleName,
+          }}
+        />
       </main>
     );
   }
