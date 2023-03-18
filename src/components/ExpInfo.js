f81198edcc2ae2d76a8fa4b94cd6f097774aa24f
RhazzXIX <jm21tonio@gmail.com>
2023-03-18 16:01:32 +0800

    Add ExpInfoForm component

--- src/components/ExpInfo.js
+++ src/components/ExpInfo.js
@@ -12,7 +12,7 @@ class ExpInfoDisplay extends Component {
             <li key={experience.experienceID}>
               <h2 className="position">{experience.position}</h2>
               <p>
-                {experience.starDate} - {experience.endDate}
+                {experience.starDate} ~ {experience.endDate}
               </p>
               <p>{experience.companyName}</p>
               <p>{experience.workLocation}</p>
@@ -25,6 +25,53 @@ class ExpInfoDisplay extends Component {
           );
         })}
       </ul>
+    );
+  }
+}
+
+class ExpInfoForm extends Component {
+  render() {
+    return (
+      <form>
+        <label htmlFor="workTitle">
+          Title:
+          <input id="workTitle" type={"text"} />
+        </label>
+        <label htmlFor="startDate">
+          Start date:
+          <input id="startDate" type={"text"} placeholder="Month & Year" />
+        </label>
+        <label htmlFor="endDate">
+          End date:
+          <input
+            id="endDate"
+            type={"text"}
+            placeholder="Month & Year || Present if still working"
+          />
+        </label>
+        <label htmlFor="companyName">
+          Company name:
+          <input id="companyName" type={"text"} />
+        </label>
+        <label htmlFor="workLoc">
+          Work location:
+          <input id="workLoc" type={"text"} />
+        </label>
+        <label htmlFor="desc1">
+          Work description:
+          <input id="desk1" type={"text"} />
+        </label>
+        <label htmlFor="desc2">
+          Additional work description:
+          <input id="desc2" type={"text"} />
+        </label>
+        <label htmlFor="desc3">
+          Additional work description:
+          <input id="desc3" type={"text"} />
+        </label>
+        <button type="submit">Save</button>
+        <button type="button">Cancel</button>
+      </form>
     );
   }
 }
@@ -81,11 +128,12 @@ class ExpInfo extends Component {
 
   render() {
     return (
-      <div>
+      <section id="experience-info">
         <h3 id="exp-head">EXPERIENCE</h3>
         <button id="testBtn">+</button>
         <ExpInfoDisplay experiences={this.state.experiences} />
-      </div>
+        <ExpInfoForm />
+      </section>
     );
   }
 }
