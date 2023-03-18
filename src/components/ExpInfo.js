d0853ba4bd72f424280a2bbd2541f0de6bbf2f53
RhazzXIX <jm21tonio@gmail.com>
2023-03-18 14:49:09 +0800

    Add state in ExpInfo component to hold data

--- src/components/ExpInfo.js
+++ src/components/ExpInfo.js
@@ -1,10 +1,40 @@
 import { Component } from "react";
 import "../styles/ExpInfo.css";
+import uniqid from 'uniqid';
+
+
 
 class ExpInfo extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      experiences: [
+        {
+          position: 'Undergraduate Research Assistant',
+          starDate: 'June 2022',
+          endDate:'Present',
+          companyName: 'Excelsior',
+          workLocation: 'Competence Kingdom',
+          workSummary: [
+            'Developing Apps that makes an impact',
+            'Teaching and developing juniors', 
+            'Making sure the applications is of great quality'
+          ],
+          workDescription: '',
+          experienceID: uniqid()
+        },
+      ],
+      experience: {
+        position: '',
+        starDate: '',
+        endDate:'',
+        companyName: '',
+        workLocation: '',
+        workSummary: [],
+        workDescription: '',
+        experienceID: uniqid()
+      }
+    };
   }
   render() {
     return (
