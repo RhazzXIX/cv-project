01246e7c6357b481c49e2b20df7fc6d7bbf823c6
RhazzXIX <jm21tonio@gmail.com>
2023-03-23 17:07:23 +0800

    Refactor GeneralInfo to functional component

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,63 +1,50 @@
-import { Component } from "react";
+import { useState } from "react";
 import "../styles/GeneralInfo.css";
 
-class GeneralInfo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isHovering: false,
-    };
-  }
+const GeneralInfo = (props) => {
+  const [isHovering, setHovering] = useState(false);
 
-  handleHover = (e) => {
-    this.setState({ isHovering: true });
+  const handleHover = (e) => {
+    setHovering(true);
   };
 
-  handleMouseleave = (e) => {
-    this.setState({
-      isHovering: false,
-    });
+  const handleMouseOut = (e) => {
+    setHovering(false);
   };
 
-  render() {
-    const { name, address, contactNum, email, gitHub, linkedIn } =
-      this.props.infos;
-    const editInfos = this.props.editInfos;
-    const mailRef = `mailto:${email}`;
-    return (
-      <div
-        id="baseInfo"
-        onMouseOver={this.handleHover}
-        onMouseLeave={this.handleMouseleave}
-      >
-        <h1 id="name">{name}</h1>
-        <h4 id="address">{address}</h4>
-        <ul id="contacts">
-          {contactNum && <li>{contactNum} </li>}
-          {email && (
-            <li>
-              || <a href={mailRef}>{email}</a>
-            </li>
-          )}
-          {gitHub && (
-            <li>
-              || <a href={gitHub}>{gitHub}</a>
-            </li>
-          )}
-          {linkedIn && (
-            <li>
-              || <a href={linkedIn}>{linkedIn}</a>
-            </li>
-          )}
-        </ul>
-        {this.state.isHovering && (
-          <button id="editBtn" onClick={editInfos}>
-            Edit
-          </button>
+  const { name, address, contactNum, email, gitHub, linkedIn } = props.infos;
+  const editInfos = props.editInfos;
+  const mailRef = `mailto:${email}`;
+  
+  return (
+    <div id="baseInfo" onMouseOver={handleHover} onMouseLeave={handleMouseOut}>
+      <h1 id="name">{name}</h1>
+      <h4 id="address">{address}</h4>
+      <ul id="contacts">
+        {contactNum && <li>{contactNum} </li>}
+        {email && (
+          <li>
+            || <a href={mailRef}>{email}</a>
+          </li>
         )}
-      </div>
-    );
-  }
-}
+        {gitHub && (
+          <li>
+            || <a href={gitHub}>{gitHub}</a>
+          </li>
+        )}
+        {linkedIn && (
+          <li>
+            || <a href={linkedIn}>{linkedIn}</a>
+          </li>
+        )}
+      </ul>
+      {isHovering && (
+        <button id="editBtn" onClick={editInfos}>
+          Edit
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default GeneralInfo;
