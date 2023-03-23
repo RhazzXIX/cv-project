3e3fc56a659b36bd70a9f81990c940d1c3e352d1
RhazzXIX <jm21tonio@gmail.com>
2023-03-23 17:58:55 +0800

    Refactor ProjectInfo to a functional component

--- src/components/ProjectInfo.js
+++ src/components/ProjectInfo.js
@@ -1,365 +1,304 @@
-import { Component } from "react";
+import { useState } from "react";
 import uniqid from "uniqid";
 import "../styles/ProjectInfo.css";
 
-class DisplayProject extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isHovering: false,
-    };
-  }
+const DisplayProject = (props) => {
+  const [isHovering, setHovering] = useState(false);
 
-  handleHover = (e) => {
-    this.setState({
-      isHovering: true,
-    });
+  const handleHover = (e) => {
+    setHovering(true);
   };
 
-  handleMouseOut = (e) => {
-    this.setState({
-      isHovering: false,
-    });
+  const handleMouseOut = (e) => {
+    setHovering(false);
   };
 
-  render() {
-    const projects = this.props.infos;
-    const deleteProject = this.props.delProj;
-    return (
-      <ul className="infoList">
-        {projects.map((project) => {
-          return (
-            <li
-              className="project infoListItem"
-              key={project.id}
-              onMouseOver={this.handleHover}
-              onMouseOut={this.handleMouseOut}
-            >
-              <h2>{project.name}</h2>
-              <p>{project.toolsUsed}</p>
-              <p>
-                {project.startDate} ~ {project.endDate}
-              </p>
-              <ul>
-                {project.summaries.map((description) => {
-                  return <li key={description.id}>{description.text}</li>;
-                })}
-              </ul>
-              {this.state.isHovering && (
-                <button
-                  type="button"
-                  onClick={deleteProject}
-                  data-proj={project.id}
-                >
-                  Del
-                </button>
-              )}
-            </li>
-          );
-        })}
-      </ul>
-    );
-  }
-}
+  const projects = props.infos;
+  const deleteProject = props.delProj;
+  return (
+    <ul className="infoList">
+      {projects.map((project) => {
+        return (
+          <li
+            className="project infoListItem"
+            key={project.id}
+            onMouseOver={handleHover}
+            onMouseOut={handleMouseOut}
+          >
+            <h2>{project.name}</h2>
+            <p>{project.toolsUsed}</p>
+            <p>
+              {project.startDate} ~ {project.endDate}
+            </p>
+            <ul>
+              {project.summaries.map((description) => {
+                return <li key={description.id}>{description.text}</li>;
+              })}
+            </ul>
+            {isHovering && (
+              <button
+                type="button"
+                onClick={deleteProject}
+                data-proj={project.id}
+              >
+                Del
+              </button>
+            )}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
-class ProjectForm extends Component {
-  render() {
-    const {
-      addProj,
-      addTools,
-      addStartDate,
-      addEndDate,
-      addProjDesc1,
-      addProjDesc2,
-      addProjDesc3,
-      addProjDesc4,
-      saveProject,
-      cancel,
-    } = this.props;
+const ProjectForm = (props) => {
+  const {
+    addProj,
+    addTools,
+    addStartDate,
+    addEndDate,
+    addProjDesc1,
+    addProjDesc2,
+    addProjDesc3,
+    addProjDesc4,
+    saveProject,
+    cancel,
+  } = props;
 
-    return (
-      <form className="projForm" onSubmit={saveProject}>
-        <label htmlFor="projectName">
-          Project Name:
-          <input onChange={addProj} id="projectName" type={"text"} />
-        </label>
-        <label htmlFor="tools">
-          Tools Used:
-          <input
-            onChange={addTools}
-            id="tools"
-            type={"text"}
-            placeholder="React.js, uniqid "
-          />
-        </label>
-        <label htmlFor="projStart">
-          Start Date:
-          <input
-            onChange={addStartDate}
-            id="projStart"
-            type={"text"}
-            placeholder="MONTH & YEAR"
-          />
-        </label>
-        <label htmlFor="projEnd">
-          Finish Date:
-          <input
-            onChange={addEndDate}
-            id="projEnd"
-            type={"text"}
-            placeholder={`"Present" if not finished`}
-          />
-        </label>
-        <label htmlFor="projDesc1">
-          Project description:
-          <input onChange={addProjDesc1} id="projDesc1" type={"text"} />
-        </label>
-        <label htmlFor="projDesc2">
-          Additional project description:
-          <input onChange={addProjDesc2} id="projDesc2" type={"text"} />
-        </label>
-        <label htmlFor="projDesc3">
-          Additional project description:
-          <input onChange={addProjDesc3} id="projDesc3" type={"text"} />
-        </label>
-        <label htmlFor="projDesc4">
-          Additional project description:
-          <input onChange={addProjDesc4} id="projDesc4" type={"text"} />
-        </label>
-        <button type="submit">Save</button>
-        <button type="button" onClick={cancel}>
-          Cancel
-        </button>
-      </form>
-    );
-  }
-}
+  return (
+    <form className="projForm" onSubmit={saveProject}>
+      <label htmlFor="projectName">
+        Project Name:
+        <input onChange={addProj} id="projectName" type={"text"} />
+      </label>
+      <label htmlFor="tools">
+        Tools Used:
+        <input
+          onChange={addTools}
+          id="tools"
+          type={"text"}
+          placeholder="React.js, uniqid "
+        />
+      </label>
+      <label htmlFor="projStart">
+        Start Date:
+        <input
+          onChange={addStartDate}
+          id="projStart"
+          type={"text"}
+          placeholder="MONTH & YEAR"
+        />
+      </label>
+      <label htmlFor="projEnd">
+        Finish Date:
+        <input
+          onChange={addEndDate}
+          id="projEnd"
+          type={"text"}
+          placeholder={`"Present" if not finished`}
+        />
+      </label>
+      <label htmlFor="projDesc1">
+        Project description:
+        <input onChange={addProjDesc1} id="projDesc1" type={"text"} />
+      </label>
+      <label htmlFor="projDesc2">
+        Additional project description:
+        <input onChange={addProjDesc2} id="projDesc2" type={"text"} />
+      </label>
+      <label htmlFor="projDesc3">
+        Additional project description:
+        <input onChange={addProjDesc3} id="projDesc3" type={"text"} />
+      </label>
+      <label htmlFor="projDesc4">
+        Additional project description:
+        <input onChange={addProjDesc4} id="projDesc4" type={"text"} />
+      </label>
+      <button type="submit">Save</button>
+      <button type="button" onClick={cancel}>
+        Cancel
+      </button>
+    </form>
+  );
+};
 
-class ProjectInfo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isHovering: false,
-      addProjInfo: false,
-      projects: [
+const ProjectInfo = () => {
+  const [isHovering, setHovering] = useState(false);
+
+  const handleHover = (e) => {
+    setHovering(true);
+  };
+
+  const handleMouseOut = (e) => {
+    setHovering(false);
+  };
+
+  const [addProjInfo, setProjInfo] = useState(false);
+  const [projects, setProjects] = useState([
+    {
+      name: "Gitlytics",
+      toolsUsed: "Python, Flask, React, PostgreSQL, Docker",
+      startDate: "June 2020",
+      endDate: "Present",
+      summaries: [
         {
-          name: "Gitlytics",
-          toolsUsed: "Python, Flask, React, PostgreSQL, Docker",
-          startDate: "June 2020",
-          endDate: "Present",
-          summaries: [
-            {
-              text: "Developed a full-stack web application using with Flask serving a REST API with React as the frontend",
-              id: uniqid(),
-            },
-            {
-              text: "Implemented Github OAuth to get data from user's repositories",
-              id: uniqid(),
-            },
-            {
-              text: "Visualized GitHub data to show collaboration",
-              id: uniqid(),
-            },
-            {
-              text: "Used Celery and Redis for asynchronous tasks",
-              id: uniqid(),
-            },
-          ],
+          text: "Developed a full-stack web application using with Flask serving a REST API with React as the frontend",
+          id: uniqid(),
+        },
+        {
+          text: "Implemented Github OAuth to get data from user's repositories",
+          id: uniqid(),
+        },
+        {
+          text: "Visualized GitHub data to show collaboration",
+          id: uniqid(),
+        },
+        {
+          text: "Used Celery and Redis for asynchronous tasks",
           id: uniqid(),
         },
       ],
-      name: "",
-      toolsUsed: "",
-      startDate: "",
-      endDate: "",
-      projDescription1: {
-        text: "",
-        id: uniqid(),
-      },
-      projDescription2: {
-        text: "",
-        id: uniqid(),
-      },
-      projDescription3: {
-        text: "",
-        id: uniqid(),
-      },
-      projDescription4: {
-        text: "",
-        id: uniqid(),
-      },
       id: uniqid(),
-    };
-  }
+    },
+  ]);
 
-  handleHover = (e) => {
-    this.setState({
-      isHovering: true,
+  const [name, setName] = useState("");
+  const [toolsUsed, setToolsUsed] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [projDescription1, setProjectDescription1] = useState({
+    text: "",
+    id: uniqid(),
+  });
+  const [projDescription2, setProjectDescription2] = useState({
+    text: "",
+    id: uniqid(),
+  });
+  const [projDescription3, setProjectDescription3] = useState({
+    text: "",
+    id: uniqid(),
+  });
+  const [projDescription4, setProjectDescription4] = useState({
+    text: "",
+    id: uniqid(),
+  });
+
+  const handleProjectNameChange = (e) => setName(e.target.value);
+
+  const handleToolsChange = (e) => setToolsUsed(e.target.value);
+
+  const handleStartDateChange = (e) => setStartDate(e.target.value);
+
+  const handleEndDateChange = (e) => setEndDate(e.target.value);
+
+  const handleDesc1Change = (e) =>
+    setProjectDescription1({
+      text: e.target.value,
+      id: uniqid(),
+    });
+  const handleDesc2Change = (e) =>
+    setProjectDescription2({
+      text: e.target.value,
+      id: uniqid(),
+    });
+
+  const handleDesc3Change = (e) =>
+    setProjectDescription3({
+      text: e.target.value,
+      id: uniqid(),
+    });
+
+  const handleDesc4Change = (e) =>
+    setProjectDescription4({
+      text: e.target.value,
+      id: uniqid(),
+    });
+
+  const addProjInfos = (e) => setProjInfo(true);
+
+  const cancelAddingInfo = (e) => setProjInfo(false);
+
+  const resetState = () => {
+    setName("");
+    setToolsUsed("");
+    setStartDate("");
+    setEndDate("");
+    setProjectDescription1({
+      text: "",
+      id: uniqid(),
+    });
+    setProjectDescription2({
+      text: "",
+      id: uniqid(),
+    });
+    setProjectDescription3({
+      text: "",
+      id: uniqid(),
+    });
+    setProjectDescription4({
+      text: "",
+      id: uniqid(),
     });
   };
 
-  handleMouseOut = (e) => {
-    this.setState({
-      isHovering: false,
-    });
-  };
-
-  handleProjectNameChange = (e) => {
-    this.setState({
-      name: e.target.value,
-    });
-  };
-
-  handleToolsChange = (e) => {
-    this.setState({
-      toolsUsed: e.target.value,
-    });
-  };
-
-  handleStartDateChange = (e) => {
-    this.setState({
-      startDate: e.target.value,
-    });
-  };
-
-  handleEndDateChange = (e) => {
-    this.setState({
-      endDate: e.target.value,
-    });
-  };
-
-  handleDesc1Change = (e) => {
-    this.setState({
-      projDescription1: {
-        text: e.target.value,
-        id: uniqid(),
-      },
-    });
-  };
-
-  handleDesc2Change = (e) => {
-    this.setState({
-      projDescription2: {
-        text: e.target.value,
-        id: uniqid(),
-      },
-    });
-  };
-
-  handleDesc3Change = (e) => {
-    this.setState({
-      projDescription3: {
-        text: e.target.value,
-        id: uniqid(),
-      },
-    });
-  };
-
-  handleDesc4Change = (e) => {
-    this.setState({
-      projDescription4: {
-        text: e.target.value,
-        id: uniqid(),
-      },
-    });
-  };
-
-  addProjInfo = (e) => {
-    this.setState({
-      addProjInfo: true,
-    });
-  };
-
-  cancelAddingInfo = (e) => {
-    this.setState({
-      addProjInfo: false,
-    });
-  };
-
-  saveProject = (e) => {
+  const saveProject = (e) => {
     e.preventDefault();
     const project = {
-      name: this.state.name,
-      toolsUsed: this.state.toolsUsed,
-      startDate: this.state.startDate,
-      endDate: this.state.endDate,
+      name,
+      toolsUsed,
+      startDate,
+      endDate,
       summaries: [
-        this.state.projDescription1,
-        this.state.projDescription2,
-        this.state.projDescription3,
-        this.state.projDescription4,
+        projDescription1,
+        projDescription2,
+        projDescription3,
+        projDescription4,
       ],
-      id: this.state.id,
-    };
-    this.setState({
-      addProjInfo: false,
-      projects: this.state.projects.concat(project),
-      name: "",
-      toolsUsed: "",
-      startDate: "",
-      endDate: "",
-      projDescription1: {
-        text: "",
-        id: uniqid(),
-      },
-      projDescription2: {
-        text: "",
-        id: uniqid(),
-      },
-      projDescription3: {
-        text: "",
-        id: uniqid(),
-      },
-      projDescription4: {
-        text: "",
-        id: uniqid(),
-      },
       id: uniqid(),
-    });
+    };
+
+    setProjects(projects.concat(project));
+    resetState();
+    setProjInfo(false);
   };
 
-  projectDelete = (e) => {
-    this.setState({
-      projects: this.state.projects.filter((project) => {
+  const projectDelete = (e) => {
+    setProjects(
+      projects.filter((project) => {
         return project.id !== e.target.dataset.proj;
-      }),
-    });
+      })
+    );
   };
 
-  render() {
-    return (
-      <section
-        id="projectInfo"
-        onMouseOver={this.handleHover}
-        onMouseOut={this.handleMouseOut}
-      >
-        <h3 id="proj-head">PROJECTS</h3>
-        {this.state.isHovering && (
-          <button className="addInfosBtn" onClick={this.addProjInfo}>
-            +
-          </button>
-        )}
-        <DisplayProject
-          infos={this.state.projects}
-          delProj={this.projectDelete}
+  return (
+    <section
+      id="projectInfo"
+      onMouseOver={handleHover}
+      onMouseOut={handleMouseOut}
+    >
+      <h3 id="proj-head">PROJECTS</h3>
+      {isHovering && (
+        <button className="addInfosBtn" onClick={addProjInfos}>
+          +
+        </button>
+      )}
+      <DisplayProject infos={projects} delProj={projectDelete} />
+      {addProjInfo && (
+        <ProjectForm
+          addProj={handleProjectNameChange}
+          addTools={handleToolsChange}
+          addStartDate={handleStartDateChange}
+          addEndDate={handleEndDateChange}
+          addProjDesc1={handleDesc1Change}
+          addProjDesc2={handleDesc2Change}
+          addProjDesc3={handleDesc3Change}
+          addProjDesc4={handleDesc4Change}
+          saveProject={saveProject}
+          cancel={cancelAddingInfo}
         />
-        {this.state.addProjInfo && (
-          <ProjectForm
-            addProj={this.handleProjectNameChange}
-            addTools={this.handleToolsChange}
-            addStartDate={this.handleStartDateChange}
-            addEndDate={this.handleEndDateChange}
-            addProjDesc1={this.handleDesc1Change}
-            addProjDesc2={this.handleDesc2Change}
-            addProjDesc3={this.handleDesc3Change}
-            addProjDesc4={this.handleDesc4Change}
-            saveProject={this.saveProject}
-            cancel={this.cancelAddingInfo}
-          />
-        )}
-      </section>
-    );
-  }
-}
+      )}
+    </section>
+  );
+};
 
 export default ProjectInfo;
